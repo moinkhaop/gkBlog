812735f9388c79788cf70e221bd8ece83ec92ef9
moinkhao1024 <moinkhao@proton.me>
2025-07-10 23:10:29 +0800

    Update ExternalImages.tsx

--- apps/gkBlog/src/contents/album/ExternalImages.tsx
+++ apps/gkBlog/src/contents/album/ExternalImages.tsx
@@ -1,9 +1,5 @@
 const externalImages: { title: string; url: string }[] = [
   {
-    title: "豆豆",
-    url: "https://cdn.qladgk.com/images/20250421214156580.jpg",
-  },
-  {
     title: "DeepSeek",
     url: "https://cdn.qladgk.com/images/20250421214156609.jpg",
   },
